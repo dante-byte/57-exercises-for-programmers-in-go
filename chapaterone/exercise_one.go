735942ddfb4f8136caa90241130639166740cef2
Dante White <559.dantewhite@gmail.com>
2020-09-06 07:23:08 -0400

    first commit

---
+++ chapaterone/exercise_one.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("Enter your first name")
+
+	var first string
+
+	fmt.Scan(&first)
+
+	fmt.Print("Hello " + first)
+
+
+	
+}
+
+
+/*
+Getting input from the user and converting it
+to something meaningful is one of the fundamental
+pieces of programming. Software developers are
+always
+turning data into information that can be
+used to make
+decisions. That data may come from the keyboard,
+a mouse,
+a touch, a swipe, or even a game controller.
+The computer
+has to react to it, process it, and do something
+useful. The exercises in this chapter will help you get
+acquainted with how to get input from the user and process
+it to produce output. You’ll build up strings, do a little
+math, and get your feet wet with the programming language
+you’re using. They’re simple problems, but they’ll help you
+build up your confidence as a programmer; the problems in the
+later chapters of the book are more complex. Each exercise has
+additional challenges you can do if you feel up to the task. If
+you’re new to programming, some of the challenges will ask you
+to use techniques you might not be familiar with yet. Feel free
+to skip them; you can always come back and do those challenges
+later. Ready? Set? Go!
+
+Hogan, Brian P.. Exercises for Programmers: 57 Challenges to Develop Your Coding Skills (p. 11). Pragmatic Bookshelf. Kindle Edition.
+*/
